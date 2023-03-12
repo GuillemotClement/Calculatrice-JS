399945f0880d4b5a4a51ad79d239bb60b61bf59d
Guillemot Clement <guillemot.clement@protonmail.com>
2023-03-12 19:49:43 +0100

    Debut choix calcul

---
+++ script.js
@@ -0,0 +1,30 @@
+// MENU CHOIX CALCUL
+// BOUCLE VERIF CHOIX USER
+do {
+    switch (choix) {
+        case 1:
+            alert("Vous avez choisis l'addition.");
+            break;
+        case 2:
+            alert("Vous avez choisis la soustraction.");
+            break;
+        case 3:
+            alert("vous avez choisis la multiplication.");
+            break;
+        case 4:
+            alert("vous avez choisis la division");
+            break;
+        
+    }
+        
+    let info = ""
+} 
+
+
+
+
+let choix = prompt("Quel calcul souhaitez-vous realiser ?\n1 - Addition\n 2 - soustraction\n 3 - Multiplication\n 4 - Division");
+        
+        
+        
+        
