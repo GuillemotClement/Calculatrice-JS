336c436e77d00b41cf02f1180c32dc4e0cac2199
GuillemotClement <guillemot.clement@protonmail.com>
2023-03-13 13:17:55 +0100

    Ajout de la saisie des nombre

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // MENU CHOIX CALCUL
 // BOUCLE VERIF CHOIX USER
 //CREATION DE LA VARIABLE CHOIX QUI CONTIENT LE CHOIX DE CALCUL DE L'USER
-let choix;
+let choix, premierNombre, deuxiemeNombre;
         
 //BOUCLE DO WHILE POUR VERIFIER QUE LA SAISIS DE L'USER EST CONFORME AUX ATTENTES
 do {
     choix = prompt("Saisir l'opération souhaitée :\n\n1 - Addition\n2 - Soustraction\n3-Multiplication\n4 - Division");
 }  while(choix != 1 && choix != 2 && choix != 3 && choix != 4);      
         
-        
+do {
+    premierNombre = prompt("Saisir le premier nombre :");
+} while(isNaN(premierNombre));
+
+do {
+    deuxiemeNombre = prompt("Saisir le deuxieme nombre :");
+} while(isNaN(deuxiemeNombre));
